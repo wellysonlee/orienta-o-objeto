23f37170794e0ec1ef76b984cc9e9b36a474c0dc
wellysonlee <martinsgwellyson@gmail.com>
2024-01-02 18:49:56 -0300

    funcoes na classe

--- Conta.php
+++ Conta.php
@@ -3,5 +3,39 @@
 class Conta {
     public $cpf;
     public $nome;
-    public $saldo;
+    public $saldo = 0;
+
+    public function sacar (float $valorASacar) : void
+    {
+        if ($valorASacar > $this->saldo) {
+            echo "Saldo indisponível.";
+            return;
+        }
+        
+        $this->saldo -= $valorASacar;
+        
+    }
+
+    public function depositar (float $valorADepositar) : void
+    {
+        if ($valorADepositar < 0) {
+            echo "O valor precisa ser positivo.";
+            return;
+        } 
+
+        $this->saldo += $valorADepositar;
+        
+    }
+
+    public function transferor (float $valorATransferir, conta $contaDestino) : void
+    {
+        if ($valorATransferir < $this->saldo) {
+            echo "Saldo indisponível.";
+            return;
+        } 
+
+        $this->sacar($valorATransferir);
+        $contaDestino->depositar($valorATransferir);
+        
+    }
 }
