fbb5cfdccf53cb0fc7c0e0e76baf6a79026cabd6
wellysonlee <martinsgwellyson@gmail.com>
2024-01-08 12:30:43 -0300

    protected e parent

--- banco.php
+++ banco.php
@@ -1,20 +1,12 @@
 <?php
 
 require_once 'src/Conta.php';
-<<<<<<< HEAD
 require_once 'src/Endereco.php';
 require_once 'src/Titular.php';
 require_once 'src/CPF.php';
 
-
-$endereco  = new Endereco('Paraipaba', 'Setor B', 'Rua Principal', '82');
+$endereco = new Endereco('Petrópolis', 'um bairro', 'minha rua', '71B');
 $wellyson = new Titular(new CPF('123.456.789-10'), 'Wellyson Martins', $endereco);
-=======
-require_once 'src/Titular.php';
-require_once 'src/CPF.php';
-
-$wellyson = new Titular(new CPF('123.456.789-10'), 'Wellyson Martins');
->>>>>>> 9c24270b4514a745076f1782f9b49779363b1dc6
 $primeiraConta = new Conta($wellyson);
 $primeiraConta->deposita(500);
 $primeiraConta->saca(300); // isso é ok
@@ -23,19 +15,11 @@ echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
 echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
 echo $primeiraConta->recuperaSaldo() . PHP_EOL;
 
-<<<<<<< HEAD
-$sara = new Titular(new CPF('698.549.548-10'), 'Sara Rodrigues', $endereco);
+$sara = new Titular(new CPF('698.549.548-10'), 'sara', $endereco);
 $segundaConta = new Conta($sara);
 var_dump($segundaConta);
 
-$outroEndereco = new Endereco('Fortaleza', 'Aldeota', 'Abolição', '45');
+$outroEndereco = new Endereco('A', 'b', 'c', '1D');
 $outra = new Conta(new Titular(new CPF('123.654.789-01'), 'Abcdefg', $outroEndereco));
-=======
-$sara = new Titular(new CPF('698.549.548-10'), 'Sara Rodrigues');
-$segundaConta = new Conta($sara);
-var_dump($segundaConta);
-
-$outra = new Conta(new Titular(new CPF('123.654.789-01'), 'Abcdefg'));
->>>>>>> 9c24270b4514a745076f1782f9b49779363b1dc6
 unset($segundaConta);
 echo Conta::recuperaNumeroDeContas();
