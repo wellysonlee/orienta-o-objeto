4e3ae7f4499fded6ce0479a8939f8bc6886927a1
wellysonlee <martinsgwellyson@gmail.com>
2024-01-09 19:38:08 -0300

    autoload

--- banco.php
+++ banco.php
@@ -1,11 +1,11 @@
 <?php
 
-require_once 'src/Conta.php';
-require_once 'src/Endereco.php';
-require_once 'src/Pessoa.php';
-require_once 'src/Titular.php';
-require_once 'src/CPF.php';
+require_once 'autoload.php';
 
+use Well\Banco\Modelo\Conta\Titular;
+use Well\Banco\Modelo\Endereco;
+use Well\Banco\Modelo\CPF;
+use Well\Banco\Modelo\Conta\Conta;
 
 $endereco = new Endereco('Petrópolis', 'um bairro', 'minha rua', '71B');
 $wellyson = new Titular(new CPF('123.456.789-10'), 'Wellyson Martins', $endereco);
@@ -15,7 +15,7 @@ $primeiraConta->saca(300); // isso é ok
 
 echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
 echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
-echo $primeiraConta->recuperaSaldo() . PHP_EOL;
+echo $primeiraConta->recuperaSaldo() . PHP_EOL; 
 
 $sara = new Titular(new CPF('698.549.548-10'), 'sara', $endereco);
 $segundaConta = new Conta($sara);
