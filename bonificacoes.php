4b3e569981bfd89ae04f64b043fded1a30f5e1d7
wellysonlee <martinsgwellyson@gmail.com>
2024-01-10 21:50:20 -0300

    bonificacoes diferentes para cargos

--- bonificacoes.php
+++ bonificacoes.php
@@ -4,25 +4,34 @@ require_once 'autoload.php';
 
 
 use Well\Banco\Service\ControladorDeBonificacoes;
-use Well\Banco\Modelo\{CPF, Funcionario};
+use Well\Banco\Modelo\{CPF};
+use Well\Banco\Modelo\Funcionario\{Funcionario, Gerente, Diretor, Desenvolvedor};
 
-$umFuncionario = new Funcionario(
+$umFuncionario = new Desenvolvedor(
     'Wellyson Martins',
     new CPF('123.456.789-10'),
     'Desenvolvedor',
     1000
 );
 
-$umaFuncionaria = new Funcionario(
+$umaFuncionaria = new Gerente(
     'Lara Lopes',
     new CPF('987.654.321-10'),
     'Gerente',
     3000
 );
 
+$umDiretor = new Diretor(
+    'Weslley Martins',
+    new CPF('781.654.848-10'),
+    'Diretor',
+    5000
+);
+
 
 $controlador = new ControladorDeBonificacoes();
 $controlador->adicionaBonificacaoDe($umFuncionario);
 $controlador->adicionaBonificacaoDe($umaFuncionaria);
+$controlador->adicionaBonificacaoDe($umDiretor);
 
 echo $controlador->recuperaTotal();
